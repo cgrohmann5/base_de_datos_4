1bd5419236e1d2bd2554f81dca860966d0731771
unknown <sistemas@VRE_LAB20513.inacap.cl>
2026-07-07 12:28:45 -0400

    Conexión con base de datos oficial

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import express from 'express';
+import cors from 'cors';
+
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}))
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en puerto ${PORT}`);
 });
